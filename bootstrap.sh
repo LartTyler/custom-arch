864204312af72332cf9c20d41f297f42e2aca2d3
Tyler Lartonoix <lord.codemonkey@gmail.com>
2021-06-18 19:25:22 -0400

    Fix AUR package installations failing

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+script_dirname=$(dirname "$0")
+
 usage() {
     echo "$0 <ROOT_PASSWORD> <PRIMARY_USER_NAME> <PRIMARY_USER_PASSWORD>"
     echo
@@ -7,6 +9,22 @@ usage() {
     echo "    ROOT_PASSWORD         - Password to assign to the root user"
     echo "    PRIMARY_USER_NAME     - The username of the primary user"
     echo "    PRIMARY_USER_PASSWORD - The password of the primary user"
+}
+
+aur_install() {
+    if [ -z "$username" ] || [ -z "$aur_root" ]; then
+        echo "Called aur_install too early for $1; cannot install with username and aur_root defined"
+
+        exit 1
+    fi
+
+    build_path="${aur_root}/$(basename -s.git "$1")"
+
+    git clone "$1" "$build_path"
+    chown -R "${username}:" "$build_path"
+
+    cd "$build_path"
+    sudo -u "${username}" makepkg -si --noconfirm
 }
 
 if [ $# -ne 3 ]; then
@@ -23,11 +41,16 @@ if [ -z "$hostname" ]; then
 fi
 
 username="$2"
+user_home="/home/${username}"
+
+mkdir -p /etc/skel/{Applications/AUR,Downloads,Pictures,Games,Documents}
+sed -i 's|^SHELL=.*|SHELL=/bin/fish|' /etc/default/useradd
 
 useradd -m -G wheel "$username"
 echo -e "root:$1\n$username:$3" | chpasswd
 
-echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/allow_wheel
+# Initially, allow members of wheel to sudo without a password so we can install packages.
+echo "%wheel ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/allow_wheel_nopasswd
 
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 hwclock --systohc
@@ -104,11 +127,32 @@ case $gpu_brand in
         ;;
 esac
 
-pacman -S --noconfirm xorg lightdm lightdm-gtk-greeter bspwm sxhkd nitrogen picom alacritty firefox arandr $gpu_driver
+pacman -S --noconfirm --needed base-devel git exa fish wget xorg lightdm lightdm-gtk-greeter bspwm sxhkd nitrogen \
+    picom alacritty firefox noto-fonts arandr $gpu_driver
 
-install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc /home/${username}/.config/bspwm/bspwmrc
-install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc /home/${username}/.config/bspwm/sxhkdrc
+install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc ${user_home}/.config/bspwm/bspwmrc
+install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc ${user_home}/.config/bspwm/sxhkdrc
 
-pacman -S exa fish
+mkdir -p "${user_home}/.config"
+
+cp -r "${script_dirname}/configs/shell/.config/**/*" "${user_home}/.config"
+cp -r "${script_dirname}/configs/gui/.config/**/*" "${user_home}/.config"
+
+aur_root=${user_home}/Applications/AUR
+
+aur_install https://aur.archlinux.org/snapd.git
+systemctl enable snapd
+systemctl start snapd
+ln -s /var/lib/snapd/snap /snap
+
+snap install gitkraken --classic
+aur_install https://aur.archlinux.org/polybar.git
+aur_install https://aur.archlinux.org/ulauncher.git
+
+chown -R "${username}:" "${user_home}"
+
+# Clear passwordless sudo for wheel members and replace it with normal sudo access
+rm /etc/sudoers.d/allow_wheel_nopasswd
+echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/allow_wheel
 
 exit
